5c186d3b0d7bdcb3d96fb60bf40b8ebe7d74b9e1
Uriel Lugo <ulugo@na-at.com.mx>
2022-09-26 14:35:50 -0500

    [Add] EjerciciosInterface javadoc

--- app/src/main/java/ejercicios/ficheros/utils/EjerciciosInterface.java
+++ app/src/main/java/ejercicios/ficheros/utils/EjerciciosInterface.java
@@ -2,9 +2,18 @@ package ejercicios.ficheros.utils;
 
 public interface EjerciciosInterface {
 
+    /**
+     * Example -> "1. WriteFile"
+     */
     String getNameExample();
 
+    /**
+     * What do -> "Write in a file 'hello world'"
+     */
     String getDescription();
 
+    /**
+     * The exercise itself
+     */
     void example();
 }
